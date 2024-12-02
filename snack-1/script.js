724dc09201b02ee5362b37ecc591347698dc4997
RobertoFloris <florisroberto99@gmail.com>
2024-12-02 16:37:39 +0100

    [fix] snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,12 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
+let nomi="";
+
+for (i in names){
+  //Scorro tutti gli elementi della lista e li stampo, quando arrivo all'ultimo elemento non stampo la virgola, ottenenedo il risultato richiesto
+  i < names.length-1 ? nomi+= `'${names[i]}', ` : nomi+= `'${names[i]}'`
+}
+
+console.log(nomi);
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
