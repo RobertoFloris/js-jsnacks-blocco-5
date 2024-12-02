fccc35c9d1a8eaef806420b7c4041b574625fe66
RobertoFloris <florisroberto99@gmail.com>
2024-12-02 18:03:11 +0100

    [fix] snack 5

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,13 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
+// filtro l'array inserendo solo i pari nel nuovo array
+const numPari = nums.filter(num =>{
+  const pari = !(num%2) 
+  return pari
+})
+
+console.log(numPari);
+
+
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
