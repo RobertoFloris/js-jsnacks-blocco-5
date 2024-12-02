2e13c169ba0caf6e7e0df1ce3bd7cd7e4f5936ce
RobertoFloris <florisroberto99@gmail.com>
2024-12-02 17:34:19 +0100

    [fix] snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,12 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
+//rimappo l'array restituendo un nuovo array incrementando ogni elemento di uno
+const numberPlusOne = numbers.map (number => {
+  return number + 1
+})
+
+console.log(numberPlusOne);
+
+
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
